9e78db6010402dd468106f4c6df595a9d3688bad
GurlFriday88 <entrotlek@gmail.com>
2018-03-03 21:35:48 -0500

    added getters and setters to properties

--- Restaurant/Restaurant.cs
+++ Restaurant/Restaurant.cs
@@ -6,10 +6,10 @@ namespace Restaurant
 {
     public class Menu
     {
-        public int id;
-        private string Name;
-        private string MenuGroup;
-        public List<MenuItem> MenuItems;
+        public int id { get; set; };
+        private string Name { get; set; };
+        private string MenuGroup { get; set; };
+        public List<MenuItem> MenuItems { get; set; };
         public DateTime DateUpdated= DateTime.Today;
 
         
@@ -18,7 +18,7 @@ namespace Restaurant
 
     public class MenuItem
     {
-        public int id;
+        public int id { get; set; };
         public string Name { get; set; }
         private string Category { get; set; }
         public string Description { get; set; }
