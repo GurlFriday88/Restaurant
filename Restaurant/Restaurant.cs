d8042489ef31e9c12e47f8674c12a0f3857980d6
GurlFriday88 <entrotlek@gmail.com>
2018-03-03 21:29:42 -0500

    added properties to classes

---
+++ Restaurant/Restaurant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Menu
+    {
+        public int id;
+        private string Name;
+        private string MenuGroup;
+        public List<MenuItem> MenuItems;
+        public DateTime DateUpdated= DateTime.Today;
+
+        
+        
+    }
+
+    public class MenuItem
+    {
+        public int id;
+        public string Name { get; set; }
+        private string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price{ get; set; }
+        public bool IsNewItem = false;
+        
+        
+        
+
+    }
+}
